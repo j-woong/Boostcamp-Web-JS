1c8b482f7c97697fc8c3063ad0921231053782b9
(김지웅) <(rlawldnd905@naver.com)>
2026-05-28 15:33:49 +0900

    입문 문제: 배열뒤집기

---
+++ Programmers/Level0/Level0_배열뒤집기.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    return num_list.reverse();
+}
+// const solution = (num_list) => num_list.reverse();
+
+// 이 코드는 원본을 훼손하지 않고 복사해서 배열을 생성함
+// const solution = (num_list) => [...num_list].reverse();
